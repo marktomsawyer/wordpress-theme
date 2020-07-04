11d3b708e48b9560ba9249c7f3e1718f15de9ee6
Mark Tom Sawyer <mark@tomsawyer.com.br>
2020-07-04 17:17:48 -0300

    Parte 02

---
+++ wp-content/themes/meu-tema/functions.php
@@ -0,0 +1,16 @@
+<?php
+
+function wp_theme_registrando_menu() {
+    register_nav_menu(
+        'menu-navegacao',
+        'Menu navegação'
+    );
+}
+
+add_action('init', 'wp_theme_registrando_menu');
+
+
+function wp_theme_adicionando_recursos_ao_tema(){
+    add_theme_support('custom-logo');
+}
+add_action('after_setup_theme', 'wp_theme_adicionando_recursos_ao_tema');
--- wp-content/themes/meu-tema/header.php
+++ wp-content/themes/meu-tema/header.php
@@ -7,7 +7,29 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?php bloginfo("name"); ?>></title>
     <?php wp_head(); ?>
+    <link rel="stylesheet" href="<?= get_template_directory_uri() . '/assets/css/normalize.css' ?>">
+    <link rel="stylesheet" href="<?= get_template_directory_uri() . '/assets/css/bootstrap.css' ?>">
+    <link rel="stylesheet" href="<?= get_template_directory_uri() . '/assets/css/header.css' ?>">
 </head>
-<body>
+<body <?php body_class(); ?>>
+
+<header class="site-header">
+    <div class="container-site">
+        <?php
+        the_custom_logo();
+        ?>
+    <nav>
+        <?php
+        wp_nav_menu(
+            array(
+                'menu' => 'menu-navegacao',
+                'menu_id' => 'menu-principal'
+            )
+        );
+        ?>
+    </nav>
+    </div>
+</header>
+
 
 
