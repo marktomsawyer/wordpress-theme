314803c22bb0bc992d27321c09029c0db6105ebe
Mark Tom Sawyer <mark@tomsawyer.com.br>
2020-07-05 20:48:13 -0300

    Parte 06

---
+++ wp-content/themes/meu-tema/front-page.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once 'header.php';
+
+$args = array(
+    'post_type' => 'banners',
+    'post_status' => 'publish',
+    'posts_per_page' => 1
+);
+
+$query = new WP_Query($args);
+if ($query->have_posts()):
+    while ($query->have_posts()): $query->the_post();
+        ?>
+        <main>
+            <div class="imagem-banner">
+                <?php the_post_thumbnail(); ?>
+            </div>
+        </main>
+
+    <?php
+    endwhile;
+endif;
+
+require_once 'footer.php';
--- wp-content/themes/meu-tema/functions.php
+++ wp-content/themes/meu-tema/functions.php
@@ -1,6 +1,7 @@
 <?php
 
-function wp_theme_registrando_menu() {
+function wp_theme_registrando_menu()
+{
     register_nav_menu(
         'menu-navegacao',
         'Menu navegação'
@@ -9,7 +10,8 @@ function wp_theme_registrando_menu() {
 
 add_action('init', 'wp_theme_registrando_menu');
 
-function wp_theme_registrando_taxonomia(){
+function wp_theme_registrando_taxonomia()
+{
     register_taxonomy(
         'paises',
         'empresas',
@@ -19,23 +21,87 @@ function wp_theme_registrando_taxonomia(){
         )
     );
 }
-add_action('init','wp_theme_registrando_taxonomia');
 
-function wp_theme_adicionando_recursos_ao_tema(){
+add_action('init', 'wp_theme_registrando_taxonomia');
+
+function wp_theme_adicionando_recursos_ao_tema()
+{
     add_theme_support('custom-logo');
     add_theme_support('post-thumbnails');
 }
+
 add_action('after_setup_theme', 'wp_theme_adicionando_recursos_ao_tema');
 
-function wp_theme_registrando_post_customizado() {
+function wp_theme_registrando_post_customizado()
+{
     register_post_type('empresas',
-    array(
-        'labels' => array('name' => 'Empresas'),
-        'public' => true,
-        'menu_position' => 0,
-        'supports' => array('title', 'editor', 'thumbnail'),
-        'menu_icon' => 'dashicon-admin-site'
-    )
+        array(
+            'labels' => array('name' => 'Empresas'),
+            'public' => true,
+            'menu_position' => 0,
+            'supports' => array('title', 'editor', 'thumbnail'),
+            'menu_icon' => 'dashicons-admin-site'
+        )
     );
 }
+
 add_action('init', 'wp_theme_registrando_post_customizado');
+
+function wp_theme_registrando_post_customizado_banner()
+{
+    register_post_type(
+        'banners',
+        array(
+            'labels' => array('name' => 'Banner'),
+            'public' => true,
+            'menu_position' => 1,
+            'menu_icon' => 'dashicons-format-image',
+            'supports' => array('title', 'thumbnail')
+        )
+    );
+}
+
+add_action('init', 'wp_theme_registrando_post_customizado_banner');
+
+function wp_theme_registrando_metabox()
+{
+    add_meta_box(
+        'mts_registrando_metabox',
+        'Texto para a home',
+        'mts_funcao_callback',
+        'banners'
+    );
+}
+
+add_action('add_meta_boxes', 'wp_theme_registrando_metabox');
+
+function mts_funcao_callback($post)
+{
+    $texto_home_1 = get_post_meta($post->ID, '_texto_home_1', true);
+    $texto_home_2 = get_post_meta($post->ID, '_texto_home_2', true);
+    ?>
+    <label for="texto_home_1">Texto 1</label>
+    <input type="text" name="texto_home_1" style="width: 100%" value="<?= $texto_home_1 ?>"/>
+    <br>
+    <br>
+    <label for="texto_home_2">Texto 2</label>
+    <input type="text" name="texto_home_2" style="width: 100%" value="<?= $texto_home_2 ?>"/>
+    <?php
+}
+
+function wp_theme_salvando_dados_metabox($post_id)
+{
+    foreach ($_POST as $key => $value) {
+        if ($key !== 'texto_home_1' && $key !== 'texto_home_2') {
+            continue;
+        }
+
+        update_post_meta(
+            $post_id,
+            '_' . $key,
+            $_POST[$key]
+        );
+    }
+}
+
+add_action('save_post', 'wp_theme_salvando_dados_metabox');
