93aaf4887849aad55cd23f6bff353d63e9064052
Mark Tom Sawyer <mark@tomsawyer.com.br>
2020-07-05 18:31:13 -0300

    Parte 04

--- wp-content/themes/meu-tema/functions.php
+++ wp-content/themes/meu-tema/functions.php
@@ -9,9 +9,33 @@ function wp_theme_registrando_menu() {
 
 add_action('init', 'wp_theme_registrando_menu');
 
+function wp_theme_registrando_taxonomia(){
+    register_taxonomy(
+        'paises',
+        'empresas',
+        array(
+            'labels' => array('name' => 'Países'),
+            'hierarchical' => true
+        )
+    );
+}
+add_action('init','wp_theme_registrando_taxonomia');
 
 function wp_theme_adicionando_recursos_ao_tema(){
     add_theme_support('custom-logo');
     add_theme_support('post-thumbnails');
 }
 add_action('after_setup_theme', 'wp_theme_adicionando_recursos_ao_tema');
+
+function wp_theme_registrando_post_customizado() {
+    register_post_type('empresas',
+    array(
+        'labels' => array('name' => 'Empresas'),
+        'public' => true,
+        'menu_position' => 0,
+        'supports' => array('title', 'editor', 'thumbnail'),
+        'menu_icon' => 'dashicon-admin-site'
+    )
+    );
+}
+add_action('init', 'wp_theme_registrando_post_customizado');
--- wp-content/themes/meu-tema/page-empresas.php
+++ wp-content/themes/meu-tema/page-empresas.php
@@ -1,3 +1,20 @@
-<h1>
-   Empresas 1
-</h1>
+<?php
+$estiloPagina = 'empresas.css';
+require_once 'header.php';
+
+$args = array('post_type' => 'empresas' );
+$query = new WP_Query($args);
+if ($query->have_posts()):
+    echo '<main class="page-empresas">';
+    echo '<ul class="lista-empresas container-site">';
+    while($query->have_posts()): $query->the_post();
+        echo '<li class="col-md-3 empresas" >';
+        the_post_thumbnail();
+        the_title('<p class="titulo-empresa">','</p>');
+        the_content();
+        echo '</li>';
+    endwhile;
+    echo '</ul>';
+    echo '</main>';
+endif;
+require_once 'footer.php';
